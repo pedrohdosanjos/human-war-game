3f30da3922cafb7bf2a6e26c55ea2daaa3ec8690
pedrohdosanjos <70206322+pedrohdosanjos@users.noreply.github.com>
2022-05-31 19:26:08 -0300

    Update Inimigo.cpp

--- sources/Inimigo.cpp
+++ sources/Inimigo.cpp
@@ -1,24 +1,9 @@
 #include "Inimigo.h"
-#include "SFML/Graphics.hpp"
-#include <Windows.h>
-#include <iostream>
 
-Inimigo::Inimigo()
+Inimigo::Inimigo():Personagem()
 {
-    vida = 100;
 }
 
 Inimigo::~Inimigo()
 {
-    vida = 0;
-}
-
-void Inimigo::setVida(int v)
-{
-    vida = v;
-}
-
-int Inimigo::getVida()
-{
-    return vida;
 }
