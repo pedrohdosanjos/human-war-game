50eeab78d75066d465015f756393d9b51a332000
pedrohdosanjos <70206322+pedrohdosanjos@users.noreply.github.com>
2022-05-31 19:27:13 -0300

    Update Soldado.cpp

--- sources/Soldado.cpp
+++ sources/Soldado.cpp
@@ -2,11 +2,14 @@
 
 Soldado::Soldado():Inimigo()
 {
-    
+    setArquivo("SoldadoParado.png");
+    setDano(100);
+    setNum_vidas(5);
+    setVelocidade(5);
+
 }
 
 Soldado::~Soldado()
 {
-	vida = 0;
 }
 
