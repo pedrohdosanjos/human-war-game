db926400d314749d728672a05f198c6766f12e2c
pedrohdosanjos <70206322+pedrohdosanjos@users.noreply.github.com>
2022-06-03 09:58:31 -0300

    Add files via upload

--- sources/Militar.cpp
+++ sources/Militar.cpp
@@ -2,11 +2,18 @@
 
 Militar::Militar():Inimigo()
 {
-   
-
+    this->initTexture();
 }
 
 Militar::~Militar()
 {
 
+}
+
+void Militar::initTexture()
+{
+	if (!this->textureSheet.loadFromFile("player.png"))
+	{
+		std::cout << "Erro ao carregar imagem" << "\n" << std::endl;
+	}
 }
